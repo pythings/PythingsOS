3090a37eea91d80db6363796d9395c1f37b4f563
Stefano Alberto Russo <stefano.russo@gmail.com>
2016-11-04 18:16:14 +0100

    Updated to test version v0.04

--- utilities/create_files_list.py
+++ utilities/create_files_list.py
@@ -2,7 +2,7 @@ from os import listdir, stat
 from os.path import isfile, join
 
 for file in listdir('./'):
-    if file!='version':
+    if file!='version' and file != '.gitignore': # Skip gitignore and do version as last (see next block)
         if not isfile(file):
             continue
         else:
