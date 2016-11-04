6de892d1efd6629a09cc8efecd9158dd3f49fa15
Stefano Alberto Russo <stefano.russo@gmail.com>
2016-11-04 18:47:00 +0100

    Fixed file sizes for release and imporved prepare_release script

--- utilities/prepare_release.sh
+++ utilities/prepare_release.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ -z "$1" ]; then
+    echo "Tell me which version"
+fi
+echo -n "$1" > version
 rm -f files.txt
 python utilities/create_files_list.py > files.txt
 python utilities/create_files_list.py > /tmp/pythings_files.txt
@@ -6,4 +10,3 @@ mv /tmp/pythings_files.txt files.txt
 python utilities/create_files_list.py > /tmp/pythings_files.txt
 mv /tmp/pythings_files.txt files.txt
 
-echo -n "$1" > version
