5560184e3e275918ee365124d7d7f31ee4e735ee
Stefano Alberto Russo <stefano.russo@gmail.com>
2016-11-26 16:11:31 +0100

    Improved prepare_release script

--- utilities/prepare_release.sh
+++ utilities/prepare_release.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 if [ -z "$1" ]; then
-    echo "Tell me which version"
+    echo "Tell me which version!! (i.e. utilities/prepare_release.sh v0.1)"
+    exit 1
 fi
-echo -n "$1" > version
-rm -f files.txt
-python utilities/create_files_list.py > files.txt
-python utilities/create_files_list.py > /tmp/pythings_files.txt
-mv /tmp/pythings_files.txt files.txt
-python utilities/create_files_list.py > /tmp/pythings_files.txt
-mv /tmp/pythings_files.txt files.txt
+
+echo "version='$1'" > common/version.py
+
+architectures=( 'Unix' 'esp8266' 'esp8266_esp-12' )
+for dir in "${architectures[@]}"
+do
+    echo $dir
+    cd $dir
+    rm -f files.txt
+    python ../utilities/create_files_list.py > files.txt
+    python ../utilities/create_files_list.py > /tmp/pythings_files.txt
+    mv /tmp/pythings_files.txt files.txt
+    python ../utilities/create_files_list.py > /tmp/pythings_files.txt
+    mv /tmp/pythings_files.txt files.txt
+    cd ..
+done
 
