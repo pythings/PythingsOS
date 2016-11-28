901b485f1004d137a30d5f1cbf80de09551c359f
Stefano Alberto Russo <stefano.russo@gmail.com>
2016-11-28 18:16:48 +0100

    Enforced App updating

--- common/updates_app.py
+++ common/updates_app.py
@@ -4,7 +4,10 @@ from utils import mv
 from http import download
 
 def update_app(version):
-    mv('/app.py','/app_bk.py')
-    download('{}/api/v1/apps/get/?version={}&token={}'.format(globals.pythings_host, version, globals.token), '/app.py')
-    logger.info('Got new, updated app')
-    return True
+    try:
+        mv('/app.py','/app_bk.py')
+        if not download('{}/api/v1/apps/get/?version={}&token={}'.format(globals.pythings_host, version, globals.token), '/app.py'):  return False
+        logger.info('Got new, updated app')
+        return True
+    except:
+        return False
