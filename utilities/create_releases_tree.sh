33ee8d6233846c16adcb10d4a139a97bdde879e0
Stefano Alberto Russo <stefano.russo@gmail.com>
2017-01-06 22:27:12 +0100

    Added automatic building and archive creation of the Python release

--- utilities/create_releases_tree.sh
+++ utilities/create_releases_tree.sh
@@ -17,6 +17,25 @@ git checkout $TAG
 
 mkdir -p $DEST/PythingsOS/$TAG
 cp -a * $DEST/PythingsOS/$TAG/
-  
+
+# Make "builds" & Zips
+
+ORIGIN=$(pwd)
+
+# MicroPython
+
+BUILD_LOCATION=$DEST/PythingsOS/$TAG/builds/Python
+# Python
+if [ -d "$BUILD_LOCATION" ]; then
+  cd $BUILD_LOCATION
+  ./build.sh
+  rm build.sh
+  rm -rf __*
+  cd .. 
+  zip -r PythingsOS_${TAG}_Python.zip Python
+fi
+
+cd $ORIGIN
+
 done
 
