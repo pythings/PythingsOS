944587a1dcfce658bff12b5dd3de2b05f0694ff1
Stefano Alberto Russo <stefano.russo@gmail.com>
2017-01-07 19:26:45 +0100

    Improved Python and MicroPython builds run.sh script

--- builds/MicroPython/run.sh
+++ builds/MicroPython/run.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-micropython boot.py
+while true; do
+    micropython boot.py
+    read -t 3 -p "Rebooting Pythings OS in 3 seconds... (type \"q\" [enter] to stop the reboot and quit) " yn
+    case $yn in
+        [Qq]* ) exit;;
+        * ) "Pythings OS reboot will continue";;
+    esac
+done
--- builds/Python/run.sh
+++ builds/Python/run.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-python3 boot.py
+while true; do
+    python3 boot.py
+    read -t 3 -p "Rebooting Pythings OS in 3 seconds... (type \"q\" [enter] to stop the reboot and quit) " yn
+    case $yn in
+        [Qq]* ) exit;;
+        * ) "Pythings OS reboot will continue";;
+    esac
+done
