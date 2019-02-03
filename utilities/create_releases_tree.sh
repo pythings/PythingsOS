11deb49d31d66815fa480a12563a8ae256a5c005
Stefano Alberto Russo <stefano.russo@gmail.com>
2019-02-03 13:02:40 +0100

    Refactored "builds" to "zips". Moved run scripts to variants main folders.

--- utilities/create_releases_tree.sh
+++ utilities/create_releases_tree.sh
@@ -8,34 +8,36 @@ fi
 # Set dest
 DEST=$1
 
-# Get tags from Git
+# For each tag
 git tag -l | while read TAG ; do
 
-    # For each tag
+    # Check it out
     echo "Checking out tag $TAG" 
     git checkout $TAG
 
+    # Create the dist tree
     mkdir -p $DEST/PythingsOS/$TAG
     cp -a * $DEST/PythingsOS/$TAG/
 
-    # Make "builds" & Zips
-
+    # Save current folder  
     ORIGIN=$(pwd)
 
-    # MicroPython
+    # For every platform where it make sense to, consolidate versions and make zips
+    for PLATFORM in Python MicroPython RaspberryPi; do   
+    echo "Consolidating and making zip archive for \"$PLATFORM\"."
+	    ZIP_LOCATION=$DEST/PythingsOS/$TAG/builds/$PLATFORM  
+	    if [ -d "$ZIP_LOCATION" ]; then
+	        cd $ZIP_LOCATION
+	        ./consolidate.sh
+	        rm consolidate.sh
+	        rm -rf __*
+	        cd .. 
+	        zip -r PythingsOS_${TAG}_${PLATFORM}.zip $PLATFORM
+	    fi
+    done
 
-    BUILD_LOCATION=$DEST/PythingsOS/$TAG/builds/Python
-    # Python
-    if [ -d "$BUILD_LOCATION" ]; then
-        cd $BUILD_LOCATION
-        ./build.sh
-        rm build.sh
-        rm -rf __*
-        cd .. 
-        zip -r PythingsOS_${TAG}_Python.zip Python
-    fi
-
-cd $ORIGIN
+    # Beck to origin folder
+    cd $ORIGIN
 
 done
 
