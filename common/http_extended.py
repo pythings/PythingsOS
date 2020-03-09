47fb0eccd435b6ae271a61ec964d7dc92cf49ef4
Stefano Alberto Russo <stefano.russo@gmail.com>
2020-03-09 15:26:49 +0100

    Added support for softuart and custom pins when using the SIM800L driver.

--- common/http_extended.py
+++ common/http_extended.py
@@ -8,6 +8,26 @@ import gc
 import network
 from utils import load_param
 from SIM800L import Modem
+from platform import platform
+
+class Uart(object):
+
+    def __init__(self, softuart):
+        self.softuart=softuart
+    
+    def write(self, string):
+        self.softuart.flush()
+        self.softuart.write(string)
+
+    def readline(self):
+        line=bytes()
+        while True:
+            char = chr(self.softuart.get())            
+            if char == '\x00':
+                return line
+            line += char.encode('utf8')
+            if char == '\n':
+                return line
 
 # Do we have a modem configured?
 try:
@@ -30,12 +50,26 @@ def at_post(url, data, dest=None):
     try:
         env.modem
     except AttributeError:
-        # Create new modem object on the right Pins    
-        env.modem = Modem(MODEM_PWKEY_PIN    = hal.MODEM_PWKEY_PIN,
-                          MODEM_RST_PIN      = hal.MODEM_RST_PIN,
-                          MODEM_POWER_ON_PIN = hal.MODEM_POWER_ON_PIN,
-                          MODEM_TX_PIN       = hal.MODEM_TX_PIN,
-                          MODEM_RX_PIN       = hal.MODEM_RX_PIN) 
+        # Do we have to load custom pins?
+        modem_pins = load_param('modem_pins', None)
+        if modem_pins:
+            MODEM_PWKEY_PIN, MODEM_RST_PIN, MODEM_POWER_ON_PIN, MODEM_TX_PIN, MODEM_RX_PIN = [int(pin) if pin != 'None' else None for pin in modem_pins.split(',')]
+
+        # Create new modem object on the right Pins
+        if platform.startswith('esp8266'):
+        
+            from machine import SOFTUART
+            uart=Uart(SOFTUART(tx = MODEM_TX_PIN if modem_pins else hal.MODEM_TX_PIN,
+                               rx = MODEM_RX_PIN if modem_pins else hal.MODEM_RX_PIN,
+                               baudrate=9600))
+            env.modem = Modem(uart)
+
+        else:        
+            env.modem = Modem(MODEM_PWKEY_PIN    = MODEM_PWKEY_PIN if modem_pins else hal.MODEM_PWKEY_PIN,
+                              MODEM_RST_PIN      = MODEM_RST_PIN if modem_pins else hal.MODEM_RST_PIN,
+                              MODEM_POWER_ON_PIN = MODEM_POWER_ON_PIN if modem_pins else hal.MODEM_POWER_ON_PIN,
+                              MODEM_TX_PIN       = MODEM_TX_PIN if modem_pins else hal.MODEM_TX_PIN,
+                              MODEM_RX_PIN       = MODEM_RX_PIN if modem_pins else hal.MODEM_RX_PIN)
         env.modem.initialize()
     
     # Always connect, as if it is already connected won't do anything
