7f5db3ea34c046eddd14bb14fa4872954b4c11c4
Stefano Alberto Russo <stefano.russo@gmail.com>
2021-01-17 17:01:39 +0100

    Fixed issue in using a local backend with the simulators.

--- tools/pythonsimulator/run.sh
+++ tools/pythonsimulator/run.sh
@@ -17,15 +17,12 @@ if [ -z "$2}" ]; then
 fi
 
 if [[ "x$3" == "xlocal" ]] ; then
-    # Do we have a PythingsCloud service running locally?
-    DOCKER_PS_LINE=$(docker ps | grep pythingscloud/proxy | grep Up)
-    if [[ "x$DOCKER_PS_LINE" != "x" ]] ; then
-        PYTHIGSCLOUD_PROXY_ID=$(echo "$DOCKER_PS_LINE" | cut -d' ' -f1)
-        PYTHIGSCLOUD_PROXY_IP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" $PYTHIGSCLOUD_PROXY_ID)
-        echo "Detected local PythingsCloud running, using its proxy IP: $PYTHIGSCLOUD_PROXY_IP"
-        be=$PYTHIGSCLOUD_PROXY_IP
-    else
-        echo "Error: you set \"local\" as backend but I cannot find any local Pythings Cloud instance running."
+    
+    # Get first network interface IP address as backend
+    be=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -n 1)
+    
+    if [[ "x$be" == "x" ]] ; then
+        echo "Error: you set \"local\" as backend but I cannot find any local IP address (?)."
         echo ""
         exit
     fi
